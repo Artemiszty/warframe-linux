eb3b0ba6e62b23aef99e16bc9d65bbc7a9c68554
NeroBurner-tux <pyro4hell@gmail.com>
2018-09-06 18:44:33 +0200

    firstrun variable was set to false after auto detection

--- Launcher.sh
+++ Launcher.sh
@@ -74,7 +74,6 @@ do_update=true
 do_cache=true
 start_game=true
 verbose=false
-firstrun=false
 #############################################################
 # parse command line arguments
 #############################################################
